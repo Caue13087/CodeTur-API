a31bcd88428e808ae5882267241b2e79bf761ef9
Caue13087 <caue13087@gmail.com>
2021-02-12 13:36:03 -0300

    API FINALIZADA

---
+++ CodeTur.Testes/Commands/CriarUsuarioCommandTestes.cs
@@ -0,0 +1,50 @@
+﻿using CodeTur.Dominio.Commands.Usuario;
+using Xunit;
+
+namespace CodeTur.Testes.Commands
+{
+    public class CriarUsuarioCommandTestes
+    {
+        [Fact]
+        public void DeveRetornarErroSeDadosInvalidos()
+        {
+            var command = new CriarUsuarioCommand();
+
+            command.Validar();
+
+            Assert.True(command.Invalid, "Os dados estão preenchidos corretamente");
+        }
+
+        [Fact]
+        public void DeveRetornarSucessoSeDadosValidosConstrutor()
+        {
+            var command = new CriarUsuarioCommand("Fernando",
+                                                  "email@email.com",
+                                                  "1234567", "",
+                                                  Comum.Enum.EnTipoUsuario.Comum
+                                                  );
+
+            command.Validar();
+
+            Assert.True(command.Valid, "Os dados estão preenchidos corretamente");
+        }
+
+        [Fact]
+        public void DeveRetornarSucessoSeDadosValidos()
+        {
+            var command = new CriarUsuarioCommand()
+            {
+                Nome = "Cauê",
+                Email = "caue.sacchitiello@email.com",
+                Senha = "1234567",
+                TipoUsuario = Comum.Enum.EnTipoUsuario.Comum
+            };
+
+            //command.Nome = "Cauê";
+
+            command.Validar();
+
+            Assert.True(command.Valid, "Os dados estão preenchidos corretamente");
+        }
+    }
+}
---
+++ CodeTur.Testes/Entidades/UsuarioTestes.cs
@@ -0,0 +1,56 @@
+﻿using CodeTur.Comum.Enum;
+using CodeTur.Dominio.Entidades;
+using Xunit;
+
+namespace CodeTur.Testes.Entidades
+{
+    public class UsuarioTestes
+    {
+        [Fact]
+        public void DeveRetornarErroSeUsuarioInvalido()
+        {
+            var usuario = new Usuario("", "caue.sacchitiello@gmail.com", "123456", EnTipoUsuario.Comum);
+            //Espera que o usuário seja invalido
+            Assert.True(usuario.Invalid, "Usuário é válido");
+        }
+
+        [Fact]
+        public void DeveRetornarErroSeEmailInvalido()
+        {
+            var usuario = new Usuario("", "caue.sacchitiello@gmail.com", "123456", EnTipoUsuario.Comum);
+            //Espera que o usuário seja invalido
+            Assert.True(usuario.Invalid, "Usuário é válido");
+        }
+
+        [Fact]
+        public void DeveRetornarSucessoSeUsuarioInvalido()
+        {
+            var usuario = new Usuario("Cauê Sacchitiello", "caue.sacchitiello@gmail.com", "123456", EnTipoUsuario.Comum);
+            usuario.AlterarUsuario("", "emailemail.com");
+
+            //Espera que o usuário seja invalido
+            Assert.True(usuario.Invalid, "Usuário é inválido");
+        }
+
+        [Fact]
+        public void DeveRetornarErroSeTelefoneUsuarioInvalido()
+        {
+            var usuario = new Usuario("Cauê Sacchitiello", "caue.sacchitiello@gmail.com", "123456", EnTipoUsuario.Comum);
+            usuario.AdicionarTelefone("959595");
+
+            //Espera que o usuário seja invalido
+            Assert.True(usuario.Invalid, "Telefone é válido");
+        }
+
+        [Fact]
+        public void DeveRetornarSucessoSeTelefoneUsuarioInvalido()
+        {
+            var usuario = new Usuario("Cauê Sacchitiello", "caue.sacchitiello@gmail.com", "123456", EnTipoUsuario.Comum);
+            usuario.AdicionarTelefone("11987546532");
+
+
+            //Espera que o usuário seja invalido
+            Assert.True(usuario.Valid, "Telefone é inválido");
+        }
+    }
+}
